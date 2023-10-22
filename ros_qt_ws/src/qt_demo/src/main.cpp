dd699eaf1548dae47d52c57cb8fc0799e05a69af
magladko <kornel.marek@gmail.com>
2023-10-23 00:01:17 +0200

    node spinning

--- ros_qt_ws/src/qt_demo/src/main.cpp
+++ ros_qt_ws/src/qt_demo/src/main.cpp
@@ -1,12 +1,18 @@
 #include "mainwindow.h"
 
 #include <QApplication>
+#include <iostream>
 #include "rclcpp/rclcpp.hpp"
 
 int main(int argc, char *argv[])
 {
+    rclcpp::init(argc, argv);
     QApplication a(argc, argv);
     MainWindow w;
+    auto node = std::make_shared<rclcpp::Node>("hello_world_node");
+    RCLCPP_INFO(node->get_logger(), "Hello world!");
+    rclcpp::spin(node);
     w.show();
+    rclcpp::shutdown();
     return a.exec();
 }
